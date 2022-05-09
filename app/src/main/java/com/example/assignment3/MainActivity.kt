338d173e0e73f6ad769ab718eb85b269fe18f645
Adam Phillips <52175162+Leg1tt@users.noreply.github.com>
2022-05-09 01:03:07 +0100

    Performed additional work on main activity and settings

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -1,32 +1,34 @@
 package com.example.assignment3
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.assignment3.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // nav_view references the bottom navigation bar in activity_main.xml
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-        val navView: BottomNavigationView = binding.navView
+        // fragment references the navigation host fragment in activity_main
+        // Getting the host fragment via the supportFragmentManager is required, as using just the
+        // FragmentContainerView isn't friendly
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Sets navigation menu links to the fragments we've created
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.coursesFragment, R.id.societiesFragment, R.id.settingsFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
